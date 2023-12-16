69eb565efab86290179abb7eb9c8ec34906f4818
Anurag <anuragsinghal6072@gmail.com>
2023-12-16 15:28:52 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user-routes';
 import adminRouter from './routes/admin-routes';
 import movieRouter from './routes/movie-routes';
 import bookingsRouter from './routes/booking-routes';
+import userRouter from './routes/user-routes';
 import cors from 'cors';
 
 dotenv.config();
